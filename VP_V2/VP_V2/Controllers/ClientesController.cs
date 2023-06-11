e05747609b3a11d065721bf8859278d9d4c1bdd0
Paula Freytes <paulafreytes@outlook.com>
2023-06-11 20:45:48 -0300

    VP1003

---
+++ VP_V2/VP_V2/Controllers/ClientesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VP_V2.Data;
+using VP_V2.Models;
+using VP_V2.Models.ModelsView;
+
+namespace VP_V2.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly VP_DBcontext vPcontext;
+
+        public ClientesController(VP_DBcontext VPcontext)
+        {
+            vPcontext = VPcontext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCliente addCliente)
+        {
+            var cliente = new Cliente()
+            {
+                Nom = addCliente.Nom,
+                Ape = addCliente.Ape,
+                Dni = addCliente.Dni,
+                Cod_Tel = addCliente.Cod_Tel,
+                Tel = addCliente.Tel,
+                Mail = addCliente.Mail
+            };
+
+            vPcontext.Clientes.Add(cliente);
+            vPcontext.SaveChanges();
+
+            return RedirectToAction("Add");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ VP_V2/VP_V2/Models/ModelsView/AddCliente.cs
@@ -0,0 +1,12 @@
+﻿namespace VP_V2.Models.ModelsView
+{
+    public class AddCliente
+    {
+        public string Nom { get; set; } = string.Empty;
+        public string Ape { get; set; } = string.Empty;
+        public string? Dni { get; set; }
+        public string? Cod_Tel { get; set; }
+        public string? Tel { get; set; }
+        public string? Mail { get; set; }
+    }
+}
