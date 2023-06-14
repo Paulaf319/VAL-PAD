f62171ab36f3906e11869b11bb90998cb15157b2
Paula Freytes <paulafreytes@outlook.com>
2023-06-14 11:24:13 -0300

    VP2001

--- VP_V2/VP_V2/Program.cs
+++ VP_V2/VP_V2/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connectionString = builder.Configuration.GetConnectionString("VP_DB");
+var connectionString = builder.Configuration.GetConnectionString("VP_V2");
 if (connectionString != null)
 {
 
